f2305f3a4dfccf8285ffde6a17dbe9bd4032850e
banglib <banglib@163.com>
2020-08-08 16:12:03 +0800

    init info

---
+++ src/main/java/com/banglib/leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -0,0 +1,42 @@
+package com.banglib.leetcode.editor.cn;
+/**
+ *@name:最长回文子串
+ *@author:banglib
+ *
+ **/
+//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。 
+//
+// 示例 1： 
+//
+// 输入: "babad"
+//输出: "bab"
+//注意: "aba" 也是一个有效答案。
+// 
+//
+// 示例 2： 
+//
+// 输入: "cbbd"
+//输出: "bb"
+// 
+// Related Topics 字符串 动态规划
+
+
+    public class LongestPalindromicSubstring{
+      public static void main(String[] args) {
+          Solution solution = new LongestPalindromicSubstring().new Solution();
+          //TODO 测试一下吧
+           
+      }
+      
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestPalindrome(String s) {
+
+        //TODO
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
